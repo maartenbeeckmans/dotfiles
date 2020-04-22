a3f5b6c8dc4cb9f9d4c716014f76450c24107d85
Maarten Beeckmans <maarten.beeckmans@student.hogent.be>
2020-04-22 23:40:39 +0200

    add backup script for dotfile

---
+++ backup-dotfiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Starting backing up config files"
+
+rsync -axvh $HOME/.zshrc ./.zshrc
+rsync -axvh $HOME/.vimrc ./.vimrc
+rsync -axvh $HOME/.config/alacritty/ ./.config/alacritty/
+rsync -axvh $HOME/.config/dunst/ ./.config/dunst/
+rsync -axvh $HOME/.config/fish ./.config/fish/
+rsync -axvh $HOME/.config/i3/ ./.config/i3/
+rsync -axvh $HOME/.config/kitty/ ./.config/kitty/
+rsync -axvh $HOME/.config/nitrogen/ ./.config/nitrogen/
+rsync -axvh $HOME/.config/nvim/ ./.config/nvim/
+rsync -axvh $HOME/.config/picom/ ./.config/picom/
+rsync -axvh $HOME/.config/polybar/ ./.config/polybar/
+rsync -axvh $HOME/.config/ranger/ ./.config/ranger/
+rsync -axvh $HOME/.config/redshift/ ./.config/redshift/
+rsync -axvh $HOME/.config/rofi/ ./.config/rofi/
+
+echo "Backing up config files complete"
