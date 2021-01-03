6d3f9d0189561e2bd52e0131902cd966dd035174
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-03 21:57:19 +0100

    Switch to wayland

--- backup-dotfiles.sh
+++ backup-dotfiles.sh
@@ -8,8 +8,10 @@ rsync -axvh $HOME/.config/alacritty/ ./.config/alacritty/
 rsync -axvh $HOME/.config/dunst/ ./.config/dunst/
 rsync -axvh $HOME/.config/fish/ ./.config/fish/
 rsync -axvh $HOME/.config/i3/ ./.config/i3/
+rsync -axvh $HOME/.config/kanshi/ ./.config/kanshi/
 rsync -axvh $HOME/.config/kitty/ ./.config/kitty/
 rsync -axvh $HOME/.config/nitrogen/ ./.config/nitrogen/
+rsync -axvh $HOME/.config/mako/ ./.config/mako/
 rsync -axvh $HOME/.config/nvim/ ./.config/nvim/
 rsync -axvh $HOME/.config/picom/ ./.config/picom/
 rsync -axvh $HOME/.config/polybar/ ./.config/polybar/
@@ -17,6 +19,9 @@ rsync -axvh $HOME/.config/qtile/ ./.config/qtile/
 rsync -axvh $HOME/.config/ranger/ ./.config/ranger/
 rsync -axvh $HOME/.config/redshift/ ./.config/redshift/
 rsync -axvh $HOME/.config/rofi/ ./.config/rofi/
+rsync -axvh $HOME/.config/sway/ ./.config/sway/
+rsync -axvh $HOME/.config/swaylock/ ./.config/swaylock/
 rsync -axvh $HOME/.config/termite/ ./.config/termite/
+rsync -axvh $HOME/.config/waybar/ ./.config/waybar/
 
 echo "Backing up config files complete"
--- restore-dotfiles.sh
+++ restore-dotfiles.sh
@@ -8,8 +8,10 @@ rsync -axvh ./.config/alacritty/ $HOME/.config/alacritty/
 rsync -axvh ./.config/dunst/ $HOME/.config/dunst/
 rsync -axvh ./.config/fish/ $HOME/.config/fish/
 rsync -axvh ./.config/i3/ $HOME/.config/i3/
+rsync -axvh ./.config/kanshi/ $HOME/.config/kanshi/
 rsync -axvh ./.config/kitty/ $HOME/.config/kitty/
 rsync -axvh ./.config/nitrogen/ $HOME/.config/nitrogen/
+rsync -axvh ./.config/mako/ $HOME/.config/mako/
 rsync -axvh ./.config/nvim/ $HOME/.config/nvim/
 rsync -axvh ./.config/picom/ $HOME/.config/picom/
 rsync -axvh ./.config/qtile/ $HOME/.config/qtile/
@@ -17,6 +19,9 @@ rsync -axvh ./.config/polybar/ $HOME/.config/polybar/
 rsync -axvh ./.config/ranger/ $HOME/.config/ranger/
 rsync -axvh ./.config/redshift/ $HOME/.config/redshift/
 rsync -axvh ./.config/rofi/ $HOME/.config/rofi/
+rsync -axvh ./.config/sway/ $HOME/.config/sway/
+rsync -axvh ./.config/swaylock/ $HOME/.config/swaylock/
 rsync -axvh ./.config/termite/ $HOME/.config/termite/
+rsync -axvh ./.config/waybar/ $HOME/.config/waybar/
 
 echo "Restoring config files complete"
